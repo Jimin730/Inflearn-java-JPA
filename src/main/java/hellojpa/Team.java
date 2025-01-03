f2d1981929d49bbf918f3434457d7188036bb2c5
jimin <dnddnd120@naver.com>
2025-01-03 14:15:56 +0900

    feat: Team 양방향 연관관계 추가

--- src/main/java/hellojpa/Team.java
+++ src/main/java/hellojpa/Team.java
@@ -1,10 +1,10 @@
 package hellojpa;
 
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Team {
@@ -13,6 +13,15 @@ public class Team {
     @Column(name = "TEAM_ID")
     private Long id;
     private String name;
+
+    //양방향 연관관계에서 주인이 아닌쪽
+    @OneToMany(mappedBy = "team") //팀은 1의 입장이니 OneToMany. member 클래스에 team과 연결되어 있어서 mappedBy = "team" (private Team team;)
+    private List<Member> members = new ArrayList<>(); //arrayList로 만들면 add할 때 널포인터가 뜨지 않아서 관례로 많이 사용된다.
+
+    public void addMember(Member member) { //양방향 관계 편의 메서드 (양쪽으로 값 세팅)
+        member.setTeam(this);
+        members.add(member);
+    }
 
     public Long getId() {
         return id;
@@ -28,5 +37,13 @@ public class Team {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public List<Member> getMembers() {
+        return members;
+    }
+
+    public void setMembers(List<Member> members) {
+        this.members = members;
     }
 }
